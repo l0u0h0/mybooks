df4a96ca7f58c5d8e0d1e07ba68e62749e2dd221
l0u0h0 <l0u0h0@gmail.com>
2022-08-13 18:20:36 +0900

    react-error-boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Add from "./pages/Add";
+import Detail from "./pages/Detail";
+import Edit from "./pages/Edit";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Signin from "./pages/Signin";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/*" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/add" element={<Add />} />
         <Route path="/book/:id" element={<Detail />} />
         <Route path="/edit/:id" element={<Edit />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
