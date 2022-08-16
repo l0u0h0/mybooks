6879ad629b25b4a64a0ad8e0088a621dd1a1c254
l0u0h0 <l0u0h0@gmail.com>
2022-08-16 22:07:58 +0900

    book_list_add

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,8 +1,14 @@
-import { BookOutlined, HomeOutlined } from "@ant-design/icons";
+import {
+  BookOutlined,
+  DeleteOutlined,
+  EditOutlined,
+  HomeOutlined,
+} from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import { BookType } from "../types";
 import moment from "moment";
 import { Button, Tooltip } from "antd";
+import styles from "./Book.module.css";
 
 interface BookProps extends BookType {}
 
@@ -13,26 +19,54 @@ const Book: React.FC<BookProps> = ({
   createdAt,
   url,
 }) => (
-  <div>
-    <div>
-      <Link to={`/book/${bookId}`}>
+  <div className={styles.book}>
+    <div className={styles.title}>
+      <Link to={`/book/${bookId}`} className={styles.link_detail_title}>
         <BookOutlined /> {title}
       </Link>
     </div>
-    <div>
-      <Link to={`/book/${bookId}`}>{author}</Link>
+    <div className={styles.author}>
+      <Link to={`/book/${bookId}`} className={styles.link_detail_author}>
+        {author}
+      </Link>
     </div>
-    <div>{moment(createdAt).format("MM-DD-YYYY hh:mm a")}</div>
-    <div>
+    <div className={styles.created}>
+      {moment(createdAt).format("MM-DD-YYYY hh:mm a")}
+    </div>
+    <div className={styles.tooltips}>
       <Tooltip title={url}>
-        <a href={url} rel="noreferrer" target="_BLANK">
+        <a
+          href={url}
+          rel="noreferrer"
+          target="_BLANK"
+          className={styles.link_url}
+        >
           <Button
             size="small"
             type="primary"
             shape="circle"
             icon={<HomeOutlined />}
+            className={styles.button_url}
           />
         </a>
+      </Tooltip>
+      <Tooltip title="Edit">
+        <Button
+          size="small"
+          shape="circle"
+          icon={<EditOutlined />}
+          className={styles.button_edit}
+        />
+      </Tooltip>
+      <Tooltip title="Delete">
+        <Button
+          size="small"
+          type="primary"
+          shape="circle"
+          danger
+          icon={<DeleteOutlined />}
+          className={styles.button_delete}
+        />
       </Tooltip>
     </div>
   </div>
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -3,28 +3,53 @@ import { useEffect } from "react";
 import { BookType } from "../types";
 import Layout from "./Layout";
 import Book from "./Book";
+import styles from "./List.module.css";
 
 interface ListProps {
   books: BookType[] | null;
   loading: boolean;
+  error: Error | null;
   getBooks: () => void;
+  logout: () => void;
 }
 
-const List: React.FC<ListProps> = ({ books, loading, getBooks }) => {
+const List: React.FC<ListProps> = ({
+  books,
+  loading,
+  getBooks,
+  error,
+  logout,
+}) => {
   useEffect(() => {
     getBooks();
   }, [getBooks]);
+
+  useEffect(() => {
+    if (error) {
+      logout();
+    }
+  }, [error, logout]);
+
   const goAdd = () => {};
-  const logout = () => {};
   return (
     <Layout>
       <PageHeader
         title={<div>Book List</div>}
         extra={[
-          <Button key="2" type="primary" onClick={goAdd}>
+          <Button
+            key="2"
+            type="primary"
+            onClick={goAdd}
+            className={styles.button}
+          >
             Add Book
           </Button>,
-          <Button key="1" type="primary" onClick={logout}>
+          <Button
+            key="1"
+            type="primary"
+            onClick={logout}
+            className={styles.button}
+          >
             Logout
           </Button>,
         ]}
@@ -43,6 +68,7 @@ const List: React.FC<ListProps> = ({ books, loading, getBooks }) => {
         showHeader={false}
         rowKey="bookId"
         pagination={false}
+        className={styles.table}
       />
     </Layout>
   );
