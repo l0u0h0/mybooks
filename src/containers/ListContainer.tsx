da5bb676b4a7b539fe9bd3a8022c9bebf01360ed
l0u0h0 <l0u0h0@gmail.com>
2022-08-17 20:56:09 +0900

    book_delete_add

--- src/containers/ListContainer.tsx
+++ src/containers/ListContainer.tsx
@@ -3,7 +3,10 @@ import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import List from "../components/List";
 import { BookType, RootState } from "../types";
-import { getBooks as getBooksSagaStart } from "../redux/modules/books";
+import {
+  getBooks as getBooksSagaStart,
+  deleteBook as deleteBookSagaStart,
+} from "../redux/modules/books";
 import { logout as logoutSagaStart } from "../redux/modules/auth";
 import { push } from "connected-react-router";
 
@@ -27,6 +30,12 @@ export default function ListContainer() {
   const goAdd = useCallback(() => {
     dispatch(push("/add"));
   }, [dispatch]);
+  const deleteBook = useCallback(
+    (bookId: number) => {
+      dispatch(deleteBookSagaStart(bookId));
+    },
+    [dispatch]
+  );
   return (
     <List
       books={books}
@@ -35,6 +44,7 @@ export default function ListContainer() {
       error={error}
       logout={logout}
       goAdd={goAdd}
+      deleteBook={deleteBook}
     />
   );
 }
--- src/redux/modules/books.ts
+++ src/redux/modules/books.ts
@@ -45,9 +45,14 @@ export default reducer;
 
 // saga
 
-export const { getBooks, addBook } = createActions("GET_BOOKS", "ADD_BOOK", {
-  prefix,
-});
+export const { getBooks, addBook, deleteBook } = createActions(
+  "GET_BOOKS",
+  "ADD_BOOK",
+  "DELETE_BOOK",
+  {
+    prefix,
+  }
+);
 
 function* getBooksSaga() {
   try {
@@ -77,7 +82,21 @@ function* addBookSaga(action: Action<BookReqType>) {
   }
 }
 
+function* deleteBookSaga(action: Action<number>) {
+  try {
+    const bookId = action.payload;
+    yield put(pending());
+    const token: string = yield select((state) => state.auth.token);
+    yield call(BookService.deleteBook, token, bookId);
+    const books: BookType[] = yield select((state) => state.books.books);
+    yield put(success(books.filter((book) => book.bookId !== bookId)));
+  } catch (error: any) {
+    yield put(fail(new Error(error?.response?.data.error || "UNKNWON_ERROR")));
+  }
+}
+
 export function* booksSaga() {
   yield takeLatest(`${prefix}/GET_BOOKS`, getBooksSaga);
   yield takeEvery(`${prefix}/ADD_BOOK`, addBookSaga);
+  yield takeEvery(`${prefix}/DELETE_BOOK`, deleteBookSaga);
 }
