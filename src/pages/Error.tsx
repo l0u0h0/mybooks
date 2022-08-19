f845fc5be808c0fe37b38e3fccb7606ae3674adb
l0u0h0 <l0u0h0@gmail.com>
2022-08-19 19:20:32 +0900

    Error_page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { FallbackProps } from "react-error-boundary";
 
-export default function Error() {
-  return (
-    <div>
-      <h1>Error</h1>
-    </div>
-  );
-}
+const Error: React.FC<FallbackProps> = ({ error }) => {
+  if (error === undefined) {
+    return <div>UnKnown Error</div>;
+  }
+  return <div>{error.message}</div>;
+};
+
+export default Error;
